f156c524746d1c4b7b018377d908136d94637fd4
brugel18@gmail.com <brugel18@gmail.com>
2014-11-14 21:11:09 -0500

    added makefile

---
+++ Camera.cpp
@@ -0,0 +1,10 @@
+ #include <opencv2/core/core.hpp>
+ #include <opencv2/highgui/highgui.hpp>
+ #include <opencv2/imgproc/imgproc.hpp>
+ #include <stdio.h>
+
+class Camera{
+    Mat image;
+    VideoCapture cam;
+
+};
